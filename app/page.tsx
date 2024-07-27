f73f46c1004ecbfd5e1537d345494aa164dcd6c2
ssk090 <shivanandasai.38@gmail.com>
2024-07-28 01:38:58 +0530

    checkpoint chapter 4.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,3 @@
-import PostButton from "@/components/post-button";
-import { Button } from "@/components/ui/button";
-import createPost from "@/server/actions/create-post";
-import getPosts from "@/server/actions/get-posts";
-
 export default async function Home() {
-  const { success, error } = await getPosts();
-
-  return (
-    <div>
-      <h1>Posts</h1>
-      {success &&
-        success.map((post) => {
-          return <div key={post.id}>{post.title}</div>;
-        })}
-      <form action={createPost}>
-        <input
-          className="bg-black"
-          type="text"
-          name="title"
-          placeholder="Title"
-        />
-        <PostButton />
-        <Button variant={"link"}> Submit</Button>
-      </form>
-    </div>
-  );
+  return <div>hey</div>;
 }
